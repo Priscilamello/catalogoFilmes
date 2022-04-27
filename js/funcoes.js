7bafff8238b03cc3d0f78395faf4ee5932cdbfc6
Priscilamello <priscilamello.8@gmail.com>
2022-04-27 18:43:56 -0300

    começando a trabalhar no js

---
+++ js/funcoes.js
@@ -0,0 +1,10 @@
+let a = Math.floor(Math.random() * 1000)
+function minhaPrimeiraFuncao(){
+    let b = Math.floor(Math.random() * 1000)
+    return `${a} + ${b} = ${a=b}`
+}
+
+//console.log(Math.floor(Math.random()* 1000))
+//console.log(a) outra opcao é chamar a variavel a
+console.log(minhaPrimeiraFuncao())
+ 
---
+++ js/variaveis.js
@@ -0,0 +1,10 @@
+//comentario no js.
+
+//var, let, const
+
+//variavel pode mudar
+
+//number, string, booleano, array,objeto
+
+//console.log(3 * '2'); //ele entende que é numero mesmo sendo string o sinal + é concatenar
+
