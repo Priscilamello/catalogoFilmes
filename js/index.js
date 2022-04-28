e5cf3601a3e9b631b67b4f25e389352193d26185
Priscilamello <priscilamello.8@gmail.com>
2022-04-28 15:10:37 -0300

    iniciando a colocar as capas do filme no JavaScript

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let listaDeFilmes = [
+    'https://imagens.ebc.com.br/70-rZMi6fTwlz8DrudsE2MzzHmU=/365x0/smart/https://radios.ebc.com.br/sites/default/files/thumbnails/image/cartaz_do_filme_laranja_mecanica.jpg',
+    'https://cinemaemcena.com.br/uploads/criticas_old/interstellar_poster5.jpg',
+    'https://m.media-amazon.com/images/I/614qzkpKLcL._AC_SX522_.jpg',
+    'https://m.media-amazon.com/images/I/71yOrLG9Z+L._AC_SX522_.jpg' ,
+    'https://upload.wikimedia.org/wikipedia/pt/thumb/4/43/Butterflyeffect_poster.jpg/240px-Butterflyeffect_poster.jpg',
+    'https://m.media-amazon.com/images/I/81AGqBcpYOL._AC_SY550_.jpg',
+    'https://br.web.img2.acsta.net/medias/nmedia/18/91/08/82/20128877.JPG',
+    'https://seuladogeek.com.br/wp-content/uploads/2022/04/Jogador-No-1.jpg',
+
+
+]
+
+let listaDeTitulos = [
+'Laranja Mecanica' ,
+'InterEstelar',
+'Truque de Mestre',
+'Cantando na Chuva',
+'Efeito Borboleta',
+'Jurassic Park',
+'Matrix',
+'Jogador N° 1'
+
+]
