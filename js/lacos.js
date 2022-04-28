bd3efc64e9101fff48153097f0410139ae58dcb6
Priscilamello <priscilamello.8@gmail.com>
2022-04-28 11:42:58 -0300

    estudando laço de repetição for para fazer tabuada

--- js/lacos.js
+++ js/lacos.js
@@ -2,6 +2,12 @@
 //for vem com contador
 //while pode se tornar infinito
 
-//* vamos construir um loop para fazer as tabuadas
+//* vamos construir um loop for para fazer as tabuadas 0 10
 
-for (let i  = 0; i <= 10; i++) {}
+for (let i  = 0; i <= 10; i++) {
+
+    for (let j = 0; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i*j}`);
+    }
+}
+
