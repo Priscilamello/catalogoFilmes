3123220634ca2c486a6cc39a70bdab8baf89d483
Priscilamello <priscilamello.8@gmail.com>
2022-04-28 11:22:11 -0300

    estudando funções

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,10 +1,32 @@
-let a = Math.floor(Math.random() * 1000)
-function minhaPrimeiraFuncao(){
-    let b = Math.floor(Math.random() * 1000)
-    return `${a} + ${b} = ${a=b}`
-}
+// let a = Math.floor(Math.random() * 1000)
+// function minhaPrimeiraFuncao(){
+//     let b = Math.floor(Math.random() * 1000)
+//     if(a>b) {
+//     return `${a} + ${b} = ${a-b}`
+// } else {
+//     console.log (`${a} + ${b} = ${a+b}`)
+// }
 
-//console.log(Math.floor(Math.random()* 1000))
-//console.log(a) outra opcao é chamar a variavel a
-console.log(minhaPrimeiraFuncao())
+    
+// }
+
+// //console.log(Math.floor(Math.random()* 1000))
+// //console.log(a) outra opcao é chamar a variavel a
+// console.log(minhaPrimeiraFuncao())
  
+//! esta é uma função anônima
+// //const minhaSegundaFuncao = function(){
+//     return 'Escrevendo minha função anônima'
+// }
+
+// console.log(minhaSegundaFuncao())
+//() isso é um parametro
+
+//!esta é uma arrow function
+
+const a = () => {
+    return 'Esta é uma arrow function'
+ }
+
+ console.log(a('Priscila'))
+
