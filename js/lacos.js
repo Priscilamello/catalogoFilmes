f3602be234c881c7941085e38e77eedc6bb42199
Priscilamello <priscilamello.8@gmail.com>
2022-04-28 11:22:36 -0300

    estudando laços de repetição

---
+++ js/lacos.js
@@ -0,0 +1,7 @@
+//! Laços de Repetição são : FOR, While e Do...While
+//for vem com contador
+//while pode se tornar infinito
+
+//* vamos construir um loop para fazer as tabuadas
+
+for (let i  = 0; i <= 10; i++) {}
